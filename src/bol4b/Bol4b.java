e7baebc886aa711c2a87bd9615f2682a66299da6
manu <manu@manu-HP-Pavilion-g6-Notebook-PC>
2015-10-15 23:59:03 +0200

    Casi terminado

--- src/bol4b/Bol4b.java
+++ src/bol4b/Bol4b.java
@@ -1,5 +1,7 @@
 package bol4b;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author manu
@@ -10,6 +12,17 @@ public class Bol4b {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     consumo obj = new consumo ();
+     
+     float litros = 50 , pGas = 1.57f, vMed, distancia;
+     String respuesta = JOptionPane.showInputDialog("Introduce velocidad media: ");
+     vMed = Float.parseFloat(respuesta);
+     String respuesta1 = JOptionPane.showInputDialog("Introduce distancia");
+     distancia = Float.parseFloat(respuesta1);
+     
+     consumo obj2 = new consumo (distancia, litros, vMed, pGas);
+     
+     JOptionPane.showMessageDialog(null, + obj2.getConsumoMedio(litros, distancia));
      
     }
     
